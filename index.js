b3dd7ac2a01a694b72afb1179f3b8f17e905deb8
davi <davisamarciel@gmail.com>
2023-08-21 21:48:58 -0300

    realoding page to button render as clickable again

--- index.js
+++ index.js
@@ -1,35 +1,58 @@
-function startApp(mediaPlayer, parentPlayer) {
-  function buttonEvents() {
-    const btn = document.createElement('button');
-    btn.textContent = 'ENABLE FULLSCREEN';
-    btn.classList.add('btn-fullscreen');
-    mediaPlayer.appendChild(btn);
 
-    let isFull = false;
-
-    btn.onclick = function() {
-      if (!isFull) {
-        isFull = true;
-        document.body.appendChild(mediaPlayer);
-        mediaPlayer.style.position = 'fixed';
-      } else {
-        isFull = false;
-        mediaPlayer.style.position = 'relative';
-        mediaPlayer.classList.add('no-fullscreen')
-        parentPlayer.appendChild(mediaPlayer)
-      }
-    }
-  }
-  buttonEvents();
+function createButton() {
+  const btn = document.createElement('div');
+  btn.innerText = 'ENABLE FULLSCREEN';
+  btn.classList.add('btn-fullscreen');
+  
+  return btn;
 };
 
-  const catchPlayer = setInterval(() => {
-    const mediaPlayer = document.querySelector('.video-viewer--container--23VX7');
-    const parentPlayer = mediaPlayer.parentNode;
+function startApp(mediaPlayer, parentPlayer) {
+  let isFull = false;
   
-    if(mediaPlayer) {
-      clearInterval(catchPlayer);
-      startApp(mediaPlayer, parentPlayer);
-    }
-  }, 1);
+  const btn = createButton();
 
+  mediaPlayer.appendChild(btn);
+
+  btn.onclick = function() {
+    if (!isFull ) {
+      isFull = true;
+      btn.innerText = 'DISABLE FULLSCREEN';
+      document.body.appendChild(mediaPlayer);
+      mediaPlayer.style.position = 'fixed';
+      document.body.style.overflow = 'hidden';
+    } else {
+      isFull = false;
+      btn.innerText = 'ENABLE FULLSCREEN';
+      mediaPlayer.style.position = 'relative';
+      document.body.style.overflow = 'auto';
+      parentPlayer.appendChild(mediaPlayer);
+    }
+  };
+
+};  
+
+function relaodPage() {
+  const reload = location.reload();
+
+  return reload;
+}
+
+const catchVideoChange = setInterval(() => {
+  const next = document.getElementById('go-to-next-item');
+  const prev = document.getElementById('go-to-previous-item');
+
+  next.addEventListener('click', relaodPage);
+  prev.addEventListener('click', relaodPage);
+
+}, 100)
+
+const catchPlayer = setInterval(() => {
+  const mediaPlayer = document.querySelector('.video-player--container--YDQRW');
+  const parentPlayer = mediaPlayer.parentNode;
+  
+  if (mediaPlayer) {
+    startApp(mediaPlayer, parentPlayer);
+    clearInterval(catchPlayer);
+  };
+}, 1);
